6761915a682fd9ba84454e1f16114657fde4bd6b
MyuB <mjycoco0@naver.com>
2022-01-23 19:32:18 +0900

    progress made, poster image changed

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+import Movie from './Movie'
+
+export default createRouter({
+  //hash, history
+  history: createWebHashHistory(),
+  //pages
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/movie/:id',
+      component: Movie
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})

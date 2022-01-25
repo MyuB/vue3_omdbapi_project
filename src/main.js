07a629010c311e7037b91e06c9fe998e0b97e331
MyuB <mjycoco0@naver.com>
2022-01-25 13:42:57 +0900

    loading components

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index.js'
 import store from './store'
+import loadImage from './plugins/loadImage'
 // index.js 라는 파일은 그냥 폴더 명만 써줘도 알아서 index.js를 찾아서 import해준다. 
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(loadImage) //$loadImage
   .mount('#app')
 
   

77080dc819f2de60c07b85c02d075f1ac29b99b2
MyuB <mjycoco0@naver.com>
2022-01-25 14:18:50 +0900

    about page contents created

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'jay',
+    email: 'mjycoco0@naver.com',
+    blog: 'https://littlejay.tistory.com/',
+    phone: '+82-10-1234-5678',
+    image: 'https://avatars.githubusercontent.com/u/52685740?s=400&u=d4e311b8531c993a48d304fdafcbf74172feb980&v=4'
+  }) 
+}

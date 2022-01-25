c238044570a85fee68368605999ce9bd3a224152
MyuB <mjycoco0@naver.com>
2022-01-25 14:39:57 +0900

    404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './Home'
 import About from './About'
 import Movie from './Movie'
+import NotFound from './NotFound.vue'
 
 export default createRouter({
   //hash, history
@@ -19,6 +20,10 @@ export default createRouter({
     {
       path: '/about',
       component: About
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
     }
   ]
 })
